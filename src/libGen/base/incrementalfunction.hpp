69799cc5134b0f7c198109f844c7e71159068f37
vihenaux <henauxvincent@gmail.com>
2022-05-02 14:54:17 +0200

    Update Incremental Function with easier solution copy

--- src/libGen/base/incrementalfunction.hpp
+++ src/libGen/base/incrementalfunction.hpp
@@ -6,7 +6,7 @@
 #include "solution.hpp"
 #include "mutation.hpp"
 
-class IncrementalFunction : public Function
+class IncrementalFunction : virtual public Function
 {
 	public:
 
@@ -15,25 +15,14 @@ class IncrementalFunction : public Function
 	// Assign an evaluation to the mutation of the last solution evaluated
 	std::uint64_t operator()(std::shared_ptr<Mutation> const& m) const;
 
-	// Needs to call 'setNewSolutionPvt' with the right template
-	virtual void setNewSolution(std::shared_ptr<Solution> const& s) const = 0;
-
-	// The following method should be overridden to avoid a copy of the mutation if possible
+	virtual void setNewSolution(std::shared_ptr<Solution> const& s) const;
 	virtual void mutateLastSolution(std::shared_ptr<Mutation> const& m) const;
 
 	protected:
 
-	// Force a specific last solution evaluated
-	template<class DerivedFromSolution>
-	void setNewSolutionPvt(std::shared_ptr<Solution> const& s)
-	{
-		std::shared_ptr<DerivedFromSolution> newSolution{new DerivedFromSolution(std::dynamic_pointer_cast<DerivedFromSolution>(s))};
-		lastSolutionEvaluated_ = newSolution;
-	}
+	virtual std::uint64_t incremental_evaluation(std::shared_ptr<Mutation> const& m) const;
 
 	private:
-
-	virtual std::uint64_t incremental_evaluation(std::shared_ptr<Mutation> const& s) const;
 
 	mutable std::shared_ptr<Solution> lastSolutionEvaluated_{nullptr};
 };
