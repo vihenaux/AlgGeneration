aa42695a83627528d7d59fed6df63910b0e5b6c0
vihenaux <henauxvincent@gmail.com>
2022-05-02 15:00:56 +0200

    Add tests to the base lib

---
+++ src/tests/base/testclasses.cpp
@@ -0,0 +1,138 @@
+#include "testclasses.hpp"
+
+// TEST MUTATION
+
+TestMutation::TestMutation(std::int8_t m) : offset_(m)
+{
+
+}
+
+std::int8_t TestMutation::getOffset() const
+{
+	return offset_;
+}
+
+void TestMutation::requiredForVTable()
+{
+
+}
+
+// TEST SOLUTION
+
+TestSolution::TestSolution() : value_(static_cast<uint8_t>(rand()%256))
+{
+
+}
+
+TestSolution::TestSolution(std::uint8_t const& v) : value_(v)
+{
+
+}
+
+void TestSolution::copy(std::shared_ptr<Solution> const& s)
+{
+	value_ = std::dynamic_pointer_cast<TestSolution>(s)->value_;
+}
+
+void TestSolution::mutate(std::shared_ptr<Mutation> const& m)
+{
+	value_ += std::dynamic_pointer_cast<TestMutation>(m)->getOffset();
+}
+
+void TestSolution::reverseMutation(std::shared_ptr<Mutation> const& m)
+{
+	value_ -= std::dynamic_pointer_cast<TestMutation>(m)->getOffset();
+}
+
+std::uint8_t TestSolution::getValue() const
+{
+	return value_;
+}
+
+// TEST FUNCTION
+
+TestFunction::~TestFunction()
+{
+
+}
+
+std::shared_ptr<Solution> TestFunction::getRandomSolution() const
+{
+	return std::make_shared<TestSolution>();
+}
+
+std::uint64_t TestFunction::evaluate(std::shared_ptr<Solution> const& s) const
+{
+	return static_cast<std::uint64_t>(std::dynamic_pointer_cast<TestSolution>(s)->getValue());
+}
+
+// TEST INCREMENTAL FUNCTIONS
+
+void TestNonOverloadedIncrementalFunction::requiredForVTable()
+{
+
+}
+
+void TestIncrementalFunction::setNewSolution(std::shared_ptr<Solution> const& s) const
+{
+	// We do not need a copy of the solution to do incremental evaluation
+	//setNewSolutionPvt(std::dynamic_pointer_cast<TestSolution>(s));
+
+	lastSolutionEvaluation_ = std::dynamic_pointer_cast<TestSolution>(s)->getValue();
+}
+
+void TestIncrementalFunction::mutateLastSolution(std::shared_ptr<Mutation> const& m) const
+{
+	lastSolutionEvaluation_ = static_cast<std::uint64_t>(
+								static_cast<std::int64_t>(lastSolutionEvaluation_) + std::dynamic_pointer_cast<TestMutation>(m)->getOffset());
+}
+
+std::uint64_t TestIncrementalFunction::incremental_evaluation(std::shared_ptr<Mutation> const& m) const
+{
+	return static_cast<std::uint64_t>(static_cast<std::int64_t>(lastSolutionEvaluation_) + std::dynamic_pointer_cast<TestMutation>(m)->getOffset());
+}
+
+// TEST NEIGHBORHOOD
+
+TestNeighborhood::~TestNeighborhood()
+{
+
+}
+
+void TestNeighborhood::setNewSolutionPvt(std::shared_ptr<Solution> const& s)
+{
+	rootSolution_->copy(s);
+}
+
+bool TestNeighborhood::neighborAvailable() const
+{
+	auto solVal = std::dynamic_pointer_cast<TestSolution>(rootSolution_)->getValue();
+
+	// Two neighbors are available, and only 1 for limit values
+	return (getNumberOfNeighborsDiscarded() < (2 - static_cast<std::uint8_t>((solVal == 255) || (solVal == 0))));
+}
+
+std::shared_ptr<Mutation> TestNeighborhood::nextNeighborPvt()
+{
+	auto m = std::make_shared<TestMutation>((nextNeighbor_) ? -1 : 1);
+	nextNeighbor_ = !nextNeighbor_;
+	return std::move(m);
+}
+
+void TestNeighborhood::acceptMutationPvt(std::shared_ptr<Mutation> const& m)
+{
+	rootSolution_->mutate(m);
+
+	switch(std::dynamic_pointer_cast<TestSolution>(rootSolution_)->getValue())
+	{
+		case 127:
+			nextNeighbor_ = false;
+			break;
+		case -128:
+			nextNeighbor_ = true;
+			break;
+		default:
+			nextNeighbor_ = static_cast<bool>(rand()%2);
+			break;
+	}
+}
