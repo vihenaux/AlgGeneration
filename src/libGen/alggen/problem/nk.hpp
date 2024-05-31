ba6282b60660df963314d1eef229153e863fff52
vihenaux <henauxvincent@gmail.com>
2024-05-31 11:56:11 +0200

    NK code

--- src/libGen/alggen/problem/nk.hpp
+++ src/libGen/alggen/problem/nk.hpp
@@ -6,38 +6,43 @@
 
 namespace alggen
 {
-    namespace problem
-    {
+namespace problem
+{
 
-        class NK : public base::Function
-        {
-            public:
+class NK : public base::Function
+{
+    public:
 
-            NK() = delete;
-            NK(std::size_t n, std::size_t k);
+    NK() = delete;
+    NK(NK const& nk);
+    NK(std::string const& file_path);
+    NK(std::size_t n, std::size_t k);
 
-            virtual void setNewSolution(std::shared_ptr<base::Solution> const& s) const final;
-            virtual void mutateLastSolution(std::shared_ptr<base::Mutation> const& m) const final;
+    virtual void setNewSolution(std::shared_ptr<base::Solution> const& s) const final;
+    virtual void mutateLastSolution(std::shared_ptr<base::Mutation> const& m) const final;
 
-            virtual std::shared_ptr<base::Function> createCopy() const final;
-            virtual void copy(std::shared_ptr<base::Function> f) final;
+    virtual std::shared_ptr<base::Function> createCopy() const final;
+    virtual void copy(std::shared_ptr<base::Function> f) final;
 
-            virtual std::shared_ptr<base::Solution> createSolution() const final;
+    virtual std::shared_ptr<base::Solution> createSolution() const final;
 
-            private:
+    private:
 
-            virtual std::uint64_t evaluate(std::shared_ptr<base::Solution> const& s) const final;
-            virtual std::uint64_t incremental_evaluation(std::shared_ptr<base::Mutation> const& m) const final;
+    virtual std::uint64_t evaluate(std::shared_ptr<base::Solution> const& s) const final;
+    virtual std::uint64_t incremental_evaluation(std::shared_ptr<base::Mutation> const& m) const final;
 
-            std::size_t n_;
-            std::size_t k_;
-            std::size_t k1_;
-            std::size_t pow2k1_;
+    std::size_t n_;
+    std::size_t k_;
+    std::size_t k1_;
+    std::size_t pow2k1_;
 
-            std::vector<std::uint32_t> matrix_;
-            std::vector<std::uint16_t> links_;
-            std::vector<std::vector<std::uint16_t>> var_in_links_;
-        };
+    std::vector<std::uint32_t> matrix_;
+    std::vector<std::uint16_t> links_;
+    std::vector<std::vector<std::uint16_t>> var_in_links_;
 
-    }
+    mutable std::uint64_t score_{0};
+    mutable solution::BitString sol_;
+};
+
+}
 }
